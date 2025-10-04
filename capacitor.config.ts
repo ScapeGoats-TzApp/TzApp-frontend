50e2931d588e89515debcfbcb65ddea84ec753c2
dandominicstaicu <dando.ds11@gmail.com>
2025-10-05 01:57:57 +0300

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.scapegoats.tzapp',
+  appName: 'TzApp',
+  webDir: 'dist/fusion-angular-tailwind-starter/browser'
+};
+
+export default config;
